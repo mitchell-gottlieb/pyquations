150a35ee9b4689e1b2537f22d9be0fbd57f65035
Mitchell Gottlieb <mgottlieb27@gmail.com>
2025-06-28 23:14:44 -0400

    Address megalinter error

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,8 @@ templates_path: list[str] = ["_templates"]
 html_theme: str = "pydata_sphinx_theme"
 html_show_sourcelink: bool = False
 html_theme_options: dict[
-    str, dict[str, str] | list[dict[str, str | bool]] | list[str]
+    str,
+    dict[str, str] | list[dict[str, str]] | list[str],
 ] = {
     "logo": {
         "text": "pyquations",
